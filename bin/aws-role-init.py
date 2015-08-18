3bb4edf31fae6679c9384cacc2c66ded0501e97c
Chris Marchesi <cmarchesi@paybyphone.com>
2015-08-18 12:32:14 -0700

    Iron out bugs, convert to boto3

--- bin/aws-role-init.py
+++ bin/aws-role-init.py
@@ -3,10 +3,10 @@
 # aws-role-init - assume an AWS role and output shell variables that will allow
 # you to assume the role.
 
-import boto
-import boto.sts
+import boto3
 import argparse
 import sys
+import textwrap
 
 class AwsRoleInit:
     """aws-role-init - assume an AWS role and output shell variables"""
@@ -21,14 +21,14 @@ class AwsRoleInit:
     def __init__(self, role_session_name, role_arn, mfa_serial=None, mfa_token=None):
         """Set up boto, etc to get the object ready"""
         try:
-            self.sts_connection = boto.sts.STSConnection()
+            self.sts_connection = boto3.client('sts')
         except boto.exception.NoAuthHandlerFound:
-            print """\
+            print textwrap.dedent("""\
             ERROR: AWS config not set up properly.
-            See http://code.google.com/p/boto/wiki/BotoConfig or
+            See http://boto3.readthedocs.org/en/latest/guide/quickstart.html or
             http://docs.aws.amazon.com/cli/latest/userguide/cli-chap-getting-started.html
             for details on how to set up boto and the AWS cli for use.
-            """
+            """)
             sys.exit(1)
         self.role_session_name = role_session_name
         self.role_arn = role_arn
@@ -37,29 +37,30 @@ class AwsRoleInit:
 
     def assume_role(self):
         """Assume the supplied role and populate the session data."""
-        self.sts_session = self.sts_connection.assume_role(self.role_arn,
-            self.role_session_name, mfa_serial_number=self.mfa_serial, mfa_token=self.mfa_token)
+        self.sts_session = self.sts_connection.assume_role(RoleArn=self.role_arn,
+            RoleSessionName=self.role_session_name, SerialNumber=self.mfa_serial,
+            TokenCode=self.mfa_token)
 
     def output_env(self):
         """Output the session data as environment variables (bash only currently)"""
-        print """\
+        print textwrap.dedent("""\
         export AWS_ACCESS_KEY_ID={0}
         export AWS_SECRET_ACCESS_KEY={1}
         export AWS_SESSION_TOKEN={2}
         """.format(self.sts_session['Credentials']['AccessKeyId'],
             self.sts_session['Credentials']['SecretAccessKey'],
-            self.sts_session['Credentials']['SessionToken'])
+            self.sts_session['Credentials']['SessionToken']))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='aws-shell-init: assume an AWS role and set environment variables')
-    parser.add_argument('--role-session-name', action='store', nargs=1, required=True,
+    parser.add_argument('--role-session-name', action='store', required=True,
         help='The name to give to the session')
-    parser.add_argument('--role-arn', action='store', nargs=1, required=True,
+    parser.add_argument('--role-arn', action='store', required=True,
         help='The ARN of the role to assume')
-    parser.add_argument('--serial-number', action='store', nargs=1, required=False,
+    parser.add_argument('--serial-number', action='store', required=False,
         help='The ARN of the MFA device being used', default=None)
-    parser.add_argument('--token-code', action='store', nargs=1, required=False,
+    parser.add_argument('--token-code', action='store', required=False,
         help='The ARN of the MFA device being used', default=None)
 
     args = parser.parse_args()
